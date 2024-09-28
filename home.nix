8f1a0ff9a7a003e3ed74471d81ed6ca450f1cb8f
T <sysarcher@users.noreply.github.com>
2024-09-28 23:44:07 +0200

    current config

--- home.nix
+++ home.nix
@@ -1,55 +1,56 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "intel";
-  home.homeDirectory = "/home/intel";
-  home.stateVersion = "22.11";
-  #home.stateVersion = "23.05";
-
-  home.packages = with pkgs; [
-    atop
-  ];
-
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    enableAutosuggestions = true;
-    enableSyntaxHighlighting = true;
-    oh-my-zsh = {
-      enable = true;
-      plugins = [ "docker-compose" "docker" ];
-      theme = "dst";
-    };
-    initExtra = ''
-      bindkey '^f' autosuggest-accept
-    '';
-  };
-
-  programs.fzf = {
-    enable = true;
-    enableZshIntegration = true;
-  };
-
-  programs.vim = {
-    enable = true;
-    #defaultEditor = true;
-    plugins = with pkgs.vimPlugins; [
-      vim-nix
-      gruvbox
-      vim-airline
+  home-manager.users.taimoor = { pkgs, ... }: {
+    home.stateVersion = "24.05";
+    home.packages = with pkgs; [
+      htop
     ];
-    extraConfig = "colorscheme desert";
-  };
-
-  programs.neovim = {
+    programs.git.enable = true;
+    programs.git = {
+      userName = "T";
+      userEmail = "sysarcher@users.noreply.github.com";
+    };
+    programs.vim = {
       enable = true;
       #defaultEditor = true;
-      #vimAlias = true;
-      plugins = [
-        pkgs.vimPlugins.vim-nix
-        pkgs.vimPlugins.gruvbox
+      plugins = with pkgs.vimPlugins; [
+        vim-nix
+        gruvbox
+        vim-airline
       ];
-      extraConfig = "colorscheme gruvbox";
+      extraConfig = "colorscheme desert";
+    };
+    programs.neovim = {
+        enable = true;
+        defaultEditor = true;
+        vimAlias = true;
+        plugins = [
+          pkgs.vimPlugins.vim-nix
+          pkgs.vimPlugins.gruvbox
+        ];
+        extraConfig = "colorscheme gruvbox";
+    };
+    programs.zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "docker-compose" "docker" ];
+        theme = "dst";
+      };
+      initExtra = ''
+        bindkey '^f' autosuggest-accept
+      '';
+    };
+  
+    programs.fzf = {
+      enable = true;
+      enableZshIntegration = true;
+    };
+
   };
-  programs.home-manager.enable = true;
 }
+
