6567a8a6fa7cfc2a865f18be258dd7af5663781c
T <sysarcher@users.noreply.github.com>
2024-09-29 00:10:45 +0200

    Update config

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,9 @@
       ./home.nix # Home manager 
     ];
 
+  # Experimental Features
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
