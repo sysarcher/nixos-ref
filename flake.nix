fd0d9a26bd59d69489d1952cc5e1a6a2a5465fbd
T <sysarcher@users.noreply.github.com>
2026-01-04 14:32:04 +0100

    move to flakes

--- flake.nix
+++ flake.nix
@@ -2,11 +2,11 @@
   description = "NixOS config";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     
     home-manager = {
-      url = "github:nix-community/home-manager/release-24.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
--- home.nix
+++ home.nix
@@ -1,111 +1,96 @@
 { config, pkgs, ... }:
 
 {
-  home-manager.users.taimoor = { pkgs, ... }: {
-    nixpkgs.config.allowUnfree = true;
-    home.stateVersion = "24.05";
-    home.packages = with pkgs; [
-      htop
-      #atop
-      #gh
-      #kubectl
-      #fluxcd
-      nodejs_24
-      rustup
-      ripgrep
-      neovim
-      clang # for nvim (nvchad plugin requires cc)
-      jujutsu
-      tree
-      bottom
-      pass
-      dig
-      unzip
-      mongodb-compass
-      ghostscript
-      groff
-      pandoc
-      tldr     
-      ngrok
-      tmux
-      unixtools.net-tools
+  home.username = "taimoor";
+  home.homeDirectory = "/home/taimoor";
+  home.stateVersion = "24.05";
+
+  nixpkgs.config.allowUnfree = true;
+
+  home.packages = with pkgs; [
+    htop
+    nodejs_24
+    rustup
+    ripgrep
+    neovim
+    clang
+    jujutsu
+    tree
+    bottom
+    pass
+    dig
+    unzip
+    mongodb-compass
+    ghostscript
+    groff
+    pandoc
+    tldr
+    ngrok
+    tmux
+    unixtools.net-tools
+  ];
+
+  home.sessionVariables = {
+    PYENV_ROOT = "$HOME/.pyenv";
+    CPPFLAGS = "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include -I${pkgs.xz.dev}/include";
+    CXXFLAGS = "-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include -I${pkgs.xz.dev}/include";
+    CFLAGS = "-I${pkgs.openssl.dev}/include";
+    LDFLAGS = "-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib -L${pkgs.xz.out}";
+    CONFIGURE_OPTS = "-with-openssl=${pkgs.openssl.dev}";
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "T";
+    userEmail = "sysarcher@users.noreply.github.com";
+  };
+
+  programs.vim = {
+    enable = true;
+    defaultEditor = true;
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      gruvbox
+      vim-airline
     ];
-    home.sessionVariables = {
-      PYENV_ROOT="$HOME/.pyenv";
-      # pyenv flags to be able to install Python
-      CPPFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include -I${pkgs.xz.dev}/include";
-      CXXFLAGS="-I${pkgs.zlib.dev}/include -I${pkgs.libffi.dev}/include -I${pkgs.readline.dev}/include -I${pkgs.bzip2.dev}/include -I${pkgs.openssl.dev}/include -I${pkgs.xz.dev}/include";
-      CFLAGS="-I${pkgs.openssl.dev}/include";
-      LDFLAGS="-L${pkgs.zlib.out}/lib -L${pkgs.libffi.out}/lib -L${pkgs.readline.out}/lib -L${pkgs.bzip2.out}/lib -L${pkgs.openssl.out}/lib -L${pkgs.xz.out}";
-      CONFIGURE_OPTS="-with-openssl=${pkgs.openssl.dev}";
-      #PYENV_VIRTUALENV_DISABLE_PROMPT="1";
-    };
-    programs.gh = {
-      # enable = true;
-      settings = {
-        git_protocol = "ssh";
-      };
-    };
-    programs.git.enable = true;
-    programs.git.settings = {
-      user.name = "T";
-      user.email = "sysarcher@users.noreply.github.com";
-    };
-    programs.vim = {
-      enable = true;
-      defaultEditor = true;
-      plugins = with pkgs.vimPlugins; [
-        vim-nix
-        gruvbox
-        vim-airline
-      ];
-      extraConfig = "colorscheme desert";
-    };
-    #programs.neovim = {
-    #    enable = true;
-    #    #defaultEditor = true;
-    #    vimAlias = true;
-    #    plugins = [
-    #      pkgs.vimPlugins.vim-nix
-    #      pkgs.vimPlugins.gruvbox
-    #    ];
-    #    extraConfig = "colorscheme gruvbox";
-    #};
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-      autosuggestion.enable = true;
-      syntaxHighlighting.enable = true;
-      shellAliases = {
-        k = "kubectl";
-      };
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "docker-compose" "docker" "direnv" ];
-        theme = "dst";
-      };
-      initContent = ''
-        bindkey '^f' autosuggest-accept
-        alias fa='flox activate'
-      '';
-    };
-  
-    programs.fzf = {
-      enable = true;
-      enableZshIntegration = true;
+    extraConfig = "colorscheme desert";
+  };
+
+  programs.zsh = {
+    enable = true;
+    enableCompletion = true;
+    autosuggestion.enable = true;
+    syntaxHighlighting.enable = true;
+
+    shellAliases = {
+      k = "kubectl";
     };
 
-    programs.kitty = {
+    oh-my-zsh = {
       enable = true;
-      font = {
-        name = "FantasqueSansM Nerd Font Mono";
-        size = 14;
-      };
-      themeFile = "Catppuccin-Macchiato";
-      settings = {
-        enable_audio_bell = false;
-      };
+      plugins = [ "docker-compose" "docker" "direnv" ];
+      theme = "dst";
     };
+
+    initContent = ''
+      bindkey '^f' autosuggest-accept
+      alias fa='flox activate'
+    '';
+  };
+
+  programs.fzf = {
+    enable = true;
+    enableZshIntegration = true;
+  };
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "FantasqueSansM Nerd Font Mono";
+      size = 14;
+    };
+    themeFile = "Catppuccin-Macchiato";
+    settings.enable_audio_bell = false;
   };
 }
 
